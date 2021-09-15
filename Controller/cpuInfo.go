73344e7694852b64457be18f9e256ab0e55e33a5
egemeric <egemeric99@gmail.com>
2021-09-15 22:57:36 +0300

    readCpu info from mem after first start.

--- Controller/cpuInfo.go
+++ Controller/cpuInfo.go
@@ -5,21 +5,28 @@ import (
 	"strings"
 )
 
-func ReadCpuInfo(w http.ResponseWriter, r *http.Request) {
-	cpu, err := FileToLines("/proc/cpuinfo")
-	Check(err)
-	specs := make(map[string][]string)
-	for _, line := range cpu {
-		if line == "" {
-			break
-		}
-		tmp := strings.Split(line, ":")
-		var splited [2][]string
-		splited[1] = strings.Split(tmp[1], " ")
-		key := strings.TrimSpace(tmp[0])
-		specs[key] = splited[1][1:]
+var CpuInfo []byte
 
+func ReadCpuInfo(w http.ResponseWriter, r *http.Request) {
+	if len(CpuInfo) < 1 {
+		cpu, err := FileToLines("/proc/cpuinfo")
+		Check(err)
+		specs := make(map[string][]string)
+		for _, line := range cpu {
+			if line == "" {
+				break
+			}
+			tmp := strings.Split(line, ":")
+			var splited [2][]string
+			splited[1] = strings.Split(tmp[1], " ")
+			key := strings.TrimSpace(tmp[0])
+			specs[key] = splited[1][1:]
+
+		}
+		CpuInfo = ConvertToJson(specs)
+		w.Write(CpuInfo)
+	} else {
+		w.Write(CpuInfo)
 	}
-	w.Write(ConvertToJson(specs))
 
 }
