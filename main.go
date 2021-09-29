25ff5fb402b31f0686140cdc7fd02946a69c47dd
egemeric <egemeric99@gmail.com>
2021-09-29 14:42:07 +0300

    minor changes.

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 	controller "proc-parse/Controller"
 )
 
+type EndPoints struct {
+	Url string
+	Fun func(w http.ResponseWriter, r *http.Request)
+}
+
 func Middleware(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -14,14 +19,19 @@ func Middleware(next http.Handler) http.Handler {
 	})
 }
 
-func init() {
+var Urls []EndPoints
 
+func init() {
+	Urls = make([]EndPoints, 2)
+	Urls[0] = EndPoints{Url: "/api/meminfo", Fun: controller.ReadMemInfo}
+	Urls[1] = EndPoints{Url: "/api/cpuinfo", Fun: controller.ReadCpuInfo}
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/api/meminfo", Middleware(http.HandlerFunc(controller.ReadMemInfo)))
-	mux.Handle("/api/cpuinfo", Middleware(http.HandlerFunc(controller.ReadCpuInfo)))
+	for _, url := range Urls {
+		mux.Handle(url.Url, Middleware(http.HandlerFunc(url.Fun)))
+	}
 	log.Println("Listening on :3000...")
 	err := http.ListenAndServe(":3000", mux)
 	log.Fatal(err)
